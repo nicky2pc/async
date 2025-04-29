508dd969a2153c9993c48cca500e8b08e0c869d7
nickdevcomp <nikolajudin023@gmail.com>
2025-04-29 14:48:18 +0500

    src + cnf +lvl1 + hld +sh +prl +ts +sh +okx +sol +tr +f +mu +0f +se

--- static/focus.js
+++ static/focus.js
@@ -5,37 +5,28 @@ const API = {
     buhForms: "/api3/buh",
 };
 
-function run() {
-    sendRequest(API.organizationList, (orgOgrns) => {
-        const ogrns = orgOgrns.join(",");
-        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
-            const orgsMap = reqsToMap(requisites);
-            sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
-                addInOrgsMap(orgsMap, analytics, "analytics");
-                sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
-                    addInOrgsMap(orgsMap, buh, "buhForms");
-                    render(orgsMap, orgOgrns);
-                });
-            });
-        });
-    });
+async function run() {
+    const ogrOgrns = (await sendRequest(API.organizationList));
+    const ogrns = ogrOgrns.join(",");
+    const [reqs, analytics, buh] = await Promise.all(
+        [
+            sendRequest(`${API.orgReqs}?ogrn=${ogrns}`),
+            sendRequest(`${API.analytics}?ogrn=${ogrns}`),
+            sendRequest(`${API.buhForms}?ogrn=${ogrns}`)]);
+    const orgsMap = reqsToMap(reqs);
+    addInOrgsMap(orgsMap, analytics, "analytics");
+    addInOrgsMap(orgsMap, buh, "buhForms");
+    render(orgsMap, ogrOgrns);
 }
-
 run();
 
-function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
-            }
-        }
-    };
-
-    xhr.send();
+async function sendRequest(url, callback) {
+    const response = await fetch(url)
+    if (response.ok && response.status < 300){
+        return await response.json()
+    } else {
+        alert('${response.status} ${response.statusText}')
+    }
 }
 
 function reqsToMap(requisites) {
